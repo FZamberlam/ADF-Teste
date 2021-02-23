42c1991448238140e38949e60e827cb6b7c3aace
Felipe Zamberlam <fzamberlam@gmail.com>
2021-02-23 17:11:29 -0300

    Add files via upload

---
+++ SQL_Scripts/Create_Tables_Dolar.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dolar_felipe].[dolar_stage_felipe] (
+cotacaoCompra VARCHAR(30), 
+cotacaoVenda VARCHAR(30), 
+dataHoraCotacao VARCHAR(30) 
+);
+
+CREATE TABLE [dolar_felipe].[dolar_final_felipe] (
+cotacaoCompra FLOAT, 
+cotacaoVenda FLOAT, 
+dataHoraCotacao DATE 
+);
---
+++ SQL_Scripts/Select_FinalToBlob.sql
@@ -0,0 +1,5 @@
+SELECT
+	CAST(cotacaoCompra as DECIMAL(6,4)) as cotacaoCompra,
+	CAST(cotacaoVenda as DECIMAL(6,4)) as cotacaoVenda,
+	dataHoraCotacao
+FROM dolar_felipe.dolar_final_felipe
